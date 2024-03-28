d680fbafd3cebe4d93b34288cd80a0bd8eb5141d
Milan Miladinovic <milan@cloudflare.com>
2024-03-27 22:28:37 -0400

    Refactor: Make `Entry::syncStatus` private
    This helps make it a bit more obvious that we don't want to touch
    `syncStatus` directly.

--- src/workerd/io/actor-cache.h
+++ src/workerd/io/actor-cache.h
@@ -407,9 +407,16 @@ private:
     // before the read completes.
     const Value value;
     EntryValueStatus valueStatus;
+
+    // This enum indicates how synchronized this entry is with storage.
+    EntrySyncStatus syncStatus = EntrySyncStatus::NOT_IN_CACHE;
   public:
     EntryValueStatus getValueStatus() const {
       return valueStatus;
+    }
+
+    inline EntrySyncStatus getSyncStatus() const {
+      return syncStatus;
     }
 
     kj::Maybe<ValuePtr> getValuePtr() const {
@@ -427,11 +434,23 @@ private:
       }
     }
 
-    // This enum indicates how synchronized this entry is with storage.
-    EntrySyncStatus syncStatus = EntrySyncStatus::NOT_IN_CACHE;
+    void setFlushing() {
+      syncStatus = EntrySyncStatus::FLUSHING;
+    }
+
+    void setNotInCache() {
+      syncStatus = EntrySyncStatus::NOT_IN_CACHE;
+    }
+
+    // Avoid using this directly. If you want to set the status to CLEAN or DIRTY, consider using
+    // the addToCleanList() and addToDirtyList() methods. If you want to set to NOT_IN_CACHE, use
+    // setNotInCache(). This helps us keep the state transitions managable.
+    void setSyncStatus(EntrySyncStatus newStatus) {
+      syncStatus = newStatus;
+    }
 
     bool isDirty() const {
-      switch(syncStatus) {
+      switch(getSyncStatus()) {
         case EntrySyncStatus::DIRTY:
         case EntrySyncStatus::FLUSHING: {
           return true;
@@ -633,14 +652,14 @@ private:
   // Add this entry to the clean list and set its status to CLEAN.
   // This doesn't do much, but it makes it easier to track what's going on.
   void addToCleanList(Lock& listLock, Entry& entryRef) {
-    entryRef.syncStatus = EntrySyncStatus::CLEAN;
+    entryRef.setSyncStatus(EntrySyncStatus::CLEAN);
     listLock->add(entryRef);
   }
 
   // Add this entry to the dirty list and set its status to DIRTY.
   // This doesn't do much, but it makes it easier to track what's going on.
   void addToDirtyList(Entry& entryRef) {
-    entryRef.syncStatus = EntrySyncStatus::DIRTY;
+    entryRef.setSyncStatus(EntrySyncStatus::DIRTY);
     dirtyList.add(entryRef);
   }
 
